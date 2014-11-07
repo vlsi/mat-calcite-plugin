ff00920c41088ab6f5759d7a1e77f6504012a6c3
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2014-11-07 15:45:30 +0300

    Add equals/hashCode/compareTo methods to HeapReference for proper grouping and sorting

--- MatCalcitePlugin/src/com/github/vlsi/mat/optiq/HeapReference.java
+++ MatCalcitePlugin/src/com/github/vlsi/mat/optiq/HeapReference.java
@@ -3,7 +3,7 @@ package com.github.vlsi.mat.optiq;
 import org.eclipse.mat.snapshot.ISnapshot;
 import org.eclipse.mat.snapshot.model.IObject;
 
-public class HeapReference {
+public class HeapReference implements Comparable<HeapReference> {
     private final ISnapshot snapshot;
     private final IObject o;
 
@@ -17,10 +17,38 @@ public class HeapReference {
     }
 
     @Override
+    public boolean equals(Object o1) {
+        if (this == o1) return true;
+        if (o1 == null || getClass() != o1.getClass()) return false;
+
+        HeapReference that = (HeapReference) o1;
+
+        if (!o.equals(that.o)) return false;
+        if (!snapshot.equals(that.snapshot)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = snapshot.hashCode();
+        result = 31 * result + o.hashCode();
+        return result;
+    }
+
+    @Override
     public String toString() {
         String classSpecific = o.getClassSpecificName();
         if (classSpecific != null)
             return classSpecific;
         return o.getDisplayName();
+    }
+
+    @Override
+    public int compareTo(HeapReference o) {
+        int cmp = this.toString().compareTo(o.toString());
+        if (cmp != 0)
+            return cmp;
+        return getIObject().getObjectId() - o.getIObject().getObjectId();
     }
 }
