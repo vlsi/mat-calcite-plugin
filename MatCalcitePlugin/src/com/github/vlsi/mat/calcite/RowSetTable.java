b464c86ebfbd5b94fcf21ebf99732329165b5345
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2017-09-14 10:05:25 +0300

    Enable context menu to browse query results
    E.g. select u.this, u.path from java.net.URL u:
    
     Menu
     +- java.net.URL.this
        +- incoming refs, outgoing refs, etc
     +- java.net.URL.path
        +- incoming refs, outgoing refs, etc

--- MatCalcitePlugin/src/com/github/vlsi/mat/calcite/RowSetTable.java
+++ MatCalcitePlugin/src/com/github/vlsi/mat/calcite/RowSetTable.java
@@ -2,16 +2,19 @@ package com.github.vlsi.mat.calcite;
 
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Types;
 
 import javax.sql.rowset.CachedRowSet;
 
 import org.eclipse.mat.query.Column;
+import org.eclipse.mat.query.ContextProvider;
 import org.eclipse.mat.query.IContextObject;
 import org.eclipse.mat.query.IResultTable;
 import org.eclipse.mat.query.ResultMetaData;
 
 public class RowSetTable implements IResultTable {
 
+	private final ResultMetaData metaData;
 	private CachedRowSet rowSet;
 	Column[] columns;
 	int idColumnPosition = -1;
@@ -21,6 +24,8 @@ public class RowSetTable implements IResultTable {
 		ResultSetMetaData md = rowSet.getMetaData();
 
 		Column[] columns = new Column[md.getColumnCount()];
+
+		ResultMetaData.Builder mdBuilder = new ResultMetaData.Builder();
 		for (int i = 0; i < columns.length; i++) {
 			String className = md.getColumnClassName(i + 1);
 			Class<?> clazz;
@@ -29,17 +34,39 @@ public class RowSetTable implements IResultTable {
 			} catch (ClassNotFoundException e) {
 				clazz = String.class;
 			}
-			columns[i] = new Column(md.getColumnName(i + 1), clazz);
+			String columnName = md.getColumnName(i + 1);
+			columns[i] = new Column(columnName, clazz);
+			if (md.getColumnType(i + 1) == Types.JAVA_OBJECT) {
+				// Most likely a HeapReference
+				final int columnPosition = i;
+				String tableName = md.getTableName(i + 1);
+				final String label;
+				if (tableName == null || tableName.isEmpty()) {
+					label = columnName;
+				} else {
+					label = tableName + "." + columnName;
+				}
+				mdBuilder.addContext(new ContextProvider(label) {
+					@Override
+					public IContextObject getContext(Object row) {
+						return RowSetTable.getContext(row, columnPosition);
+					}
+				});
+				if (idColumnPosition == -1) {
+					// Use first object column as context provider (e.g. in case "this" column is missing)
+					idColumnPosition = i;
+				}
+			}
 			if (idColumnPosition == -1 && "this".equals(columns[i].getLabel()))
 				idColumnPosition = i;
 		}
+		this.metaData = mdBuilder.build();
 		this.columns = columns;
 	}
 
 	@Override
 	public ResultMetaData getResultMetaData() {
-
-		return null;
+		return metaData;
 	}
 
 	@Override
@@ -56,17 +83,26 @@ public class RowSetTable implements IResultTable {
 
 	@Override
 	public IContextObject getContext(final Object row) {
-		if (row == null || !(row instanceof Object[]) || idColumnPosition == -1)
+		if (idColumnPosition == -1) {
+			return null;
+		}
+		return getContext(row, idColumnPosition);
+	}
+
+	private static IContextObject getContext(final Object row, final int columnPosition) {
+		if (row == null || !(row instanceof Object[]))
 			return null;
 		final Object[] data = (Object[]) row;
-		if (idColumnPosition >= data.length)
+		if (columnPosition >= data.length)
 			return null;
+		final Object ref = data[columnPosition];
+		if (!(ref instanceof HeapReference)) {
+			return null;
+		}
 		return new IContextObject() {
-
 			@Override
 			public int getObjectId() {
-//				return (Integer) data[idColumnPosition];
-				return ((HeapReference)data[idColumnPosition]).getIObject().getObjectId();
+				return ((HeapReference) ref).getIObject().getObjectId();
 			}
 		};
 	}
